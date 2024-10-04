ef717ae4e7af8ef9e57c07328883ce7581a03313
bakir004 <bakir.cinjarevic@gmail.com>
2024-10-04 17:22:16 +0200

    asd

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.post.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    const post = await ctx.db.post.findFirst({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return post ?? null;
+  }),
+
+  deleteLatest: publicProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      const success = await ctx.db.post.deleteMany({
+        where: {
+          name: input.name,
+        },
+      });
+    }),
+});
