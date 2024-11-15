697592f675b61c65357cda016a977404b9b9d614
bakir004 <bakir.cinjarevic@gmail.com>
2024-11-15 16:27:37 +0100

    glupi kompajler

--- src/lib/testJsonFormatter.ts
+++ src/lib/testJsonFormatter.ts
@@ -1,22 +1,22 @@
-export const testJsonFormatter = (json: string) => {
-    try {
-        const testJson = JSON.parse(json);
-        const formattedTestJson = {
-            tests: []
-        };
-        const tests = testJson.tests;
-        item?.tools[0] &&
-        typeof item.tools[0] !== "string" &&
-        item.tools[0].patch?.[1]?.position === "above_main") ||
-      (item?.tools[0] &&
-        typeof item.tools[0] !== "string" &&
-        item.tools[0].patch?.[1]?.position === "top_of_file")
-        ? (currentCode +=
-            item?.tools[0] &&
-            typeof item.tools[0] !== "string" &&
-            item.tools[0].patch?.[1]?.code + "\n"
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
+// export const testJsonFormatter = (json: string) => {
+//     try {
+//         const testJson = JSON.parse(json);
+//         const formattedTestJson = {
+//             tests: []
+//         };
+//         const tests = testJson.tests;
+//         item?.tools[0] &&
+//         typeof item.tools[0] !== "string" &&
+//         item.tools[0].patch?.[1]?.position === "above_main") ||
+//       (item?.tools[0] &&
+//         typeof item.tools[0] !== "string" &&
+//         item.tools[0].patch?.[1]?.position === "top_of_file")
+//         ? (currentCode +=
+//             item?.tools[0] &&
+//             typeof item.tools[0] !== "string" &&
+//             item.tools[0].patch?.[1]?.code + "\n"
+//         return true;
+//     } catch (e) {
+//         return false;
+//     }
+// }
